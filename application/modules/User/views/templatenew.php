5a2d02a7e1ad2057c4adc278f68443c7bae403f3
rizky primadona <primadona.rizky22@gmail.com>
2018-11-28 22:34:53 +0700

    k

--- application/modules/User/views/templatenew.php
+++ application/modules/User/views/templatenew.php
@@ -6,12 +6,13 @@
 <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('assets/img/logo_pyk.png') ?>" />
   <link rel="icon" type="image/png" href="<?php echo base_url('assets/img/logo_pyk.png') ?>" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>SODAP | KOTA PAYAKUMBUH sumbar</title>
+  <title>SODAP | KOTA PAYAKUMBUH</title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
   <meta name="viewport" content="width=device-width" />
   <!-- Canonical SEO -->
   <link rel="canonical" href="http://sodap.payakumbuhkota.go.id" />
   <!--  Social tags      -->
+  ok
   <meta name="keywords" content="kota payakumbuh, aplikasi kota payakumbuh, goverment kota payakumbuh, kominfo kota payakumbuh, aplikasi kominfo payakumbuh, dinas kominfo kota payakumbuh, sodap kota payakumbuh, payakumbuh sumatra barat, pemerintah kota payakumbuh, sodap, smart city kota payakumbuh, payakumbuh go online,kominfo kota payakumbuh sumatra barat">
   <meta name="description" content="Aplikasi pemerintahan Kota Payakumbuh, yang di kembangkan Dinas Kominfo Kota Payakumbuh.">
   <!-- Schema.org markup for Google+ -->
